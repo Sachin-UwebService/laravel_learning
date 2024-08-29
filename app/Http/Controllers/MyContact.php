8322bef4e973f38f1de16577fa82e2453f46a66d
Sachin <sachinpassion150@gmail.com>
2024-08-29 11:06:34 +0530

    first comit

---
+++ app/Http/Controllers/MyContact.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyContact extends Controller
+{
+    function add_city(){
+
+        echo "This is Muzaffarpur city";
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ipsum illum sint est? Omnis odit tenetur, illum voluptatem dolor cum.
+</body>
+</html>
